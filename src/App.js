abfe0fbb5e0c9b0e307853aec75f3cf46ba2e453
run-after <run.after@protonmail.com>
2021-07-16 10:47:41 -0500

    Add Login page and a check for expired token on startup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './styles/App.css';
+import Login from './components/Login';
+import { useEffect } from 'react';
+import moment from 'moment';
 
 function App() {
+
+  useEffect(() => {
+    // Check if token is expired. If so, remove token
+    if (localStorage.getItem('token')) {
+      const tokenExpiration = JSON.parse(localStorage.getItem('token')).expires;
+      if (moment(tokenExpiration) < moment()) {
+        localStorage.removeItem('token');
+      };
+    };
+
+  }, [])
+
   return (
     <div className="App">
-      Social
+      <Login />
     </div>
   );
-}
+
+};
 
 export default App;
