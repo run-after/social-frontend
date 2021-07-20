7edd931e94536e8ecaba7c26a504ad0564697beb
run-after <run.after@protonmail.com>
2021-07-20 10:38:52 -0500

    Add API calls to requested friends and to request friend and display data based on status of friend requests

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,34 +3,88 @@ import { useState, useEffect } from 'react';
 
 function Users() {
 
+  const token = JSON.parse(localStorage.getItem('token'));
+
+  const [friendRequested, setFriendRequested] = useState({ data: [] });
+  const [requestedFriends, setRequestedFriends] = useState({ data: [] });
+  
+  const requestFriend = (person) => {
+    // Make call to api to create new friend request
+    fetch(`${process.env.REACT_APP_API_DOMAIN}/friendRequests/${person._id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token.token}`
+      }
+    }).then((initRes) => {
+      initRes.json().then(() => {
+        // Update state to show that friend was requested
+        let tempRequested = requestedFriends.data;
+        tempRequested.push(person._id);
+        setRequestedFriends({ data: tempRequested });
+      })
+    });
+  };
+
   const [users, setUsers] = useState(null);
 
   useEffect(() => {
-    const token = JSON.parse(localStorage.getItem('token')).token;
-
+    // Make api call to get all users
     fetch(`${process.env.REACT_APP_API_DOMAIN}/users`,
       {
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
+          'Authorization': `Bearer ${token.token}`
         }
       }
-    ).then((res) => {
-      res.json().then((res) => {
+    ).then((initRes) => {
+      initRes.json().then((res) => {
         setUsers(res);
       });
     });
-  }, []);
-
+    // Get all friend requests that apply to current user
+    fetch(`${process.env.REACT_APP_API_DOMAIN}/friendRequests`, {
+      headers: {
+        'Authorization': `Bearer ${token.token}`
+      }
+    }).then((initRes) => {
+      initRes.json().then((res) => {
+        let friendRequestedList = [];
+        let requestedFriendList = [];
+        res.forEach((request) => {
+          if (request.requester === token.user) {
+            requestedFriendList.push(request.requested);
+          } else if (request.requested === token.user) {
+            friendRequestedList.push(request.requester);
+          };
+        });
+        setRequestedFriends({ data: requestedFriendList });
+        setFriendRequested({ data: friendRequestedList });
+      });
+    });
+  }, [token.user, token.token]);
 
   return (
     <div className='users-page'>
+      <div className='left-column'>
+        Whatever
+      </div>
       {users && users.map((user) => {
-        return (
-          <div key={user._id} className='user'>
-            {user.firstName}
-          </div>
-        );
+        if (user._id !== token.user) {
+          return (
+            <div key={user._id} className='user'>
+              <div className='user-image'>placeholder</div>
+              <h5 className='user-name'>{`${user.firstName} ${user.lastName}`}</h5>
+              {
+                ((requestedFriends.data.includes(user._id) &&
+                  <div className='requested-block'>Friend requested</div>) ||
+                (friendRequested.data.includes(user._id) &&
+                  <button className='friend-requested btn'>Accept request</button>)) ||
+                <button className='add-friend-button btn' onClick={() => requestFriend(user)}>Add friend</button> 
+              }
+            </div>
+          );
+        }
       })}
     </div>
   );
