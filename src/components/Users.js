22e4bc6b64ff202019d92b42b6119530e15295f3
run-after <run.after@protonmail.com>
2021-07-21 10:29:32 -0500

    Add method to accept friend request and display status on user card

--- src/components/Users.js
+++ src/components/Users.js
@@ -27,6 +27,62 @@ function Users() {
     });
   };
 
+  const acceptRequest = (person) => {
+    const currentUser = JSON.parse(localStorage.getItem('token')).user;
+
+    // Add current user to person who requested friendship's friend list
+    person.friends.push(currentUser._id);
+    fetch(`${process.env.REACT_APP_API_DOMAIN}/users/${person._id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token.token}`
+      },
+      body: JSON.stringify(person)
+    });
+
+    // Add person who requested friendship to current user's friend list
+    currentUser.friends.push(person._id);
+    fetch(`${process.env.REACT_APP_API_DOMAIN}/users/${currentUser._id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token.token}`
+      },
+      body: JSON.stringify(currentUser)
+    }).then((res) => {
+      res.json().then((res) => {
+        let updatedToken = JSON.parse(localStorage.getItem('token'));
+        updatedToken.user = res;
+        localStorage.setItem('token', JSON.stringify(updatedToken));
+      });
+    });
+
+    // Find friend request in database
+    fetch(`${process.env.REACT_APP_API_DOMAIN}/friendRequests`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token.token}`
+      },
+    }).then((initRes) => {
+      initRes.json().then((res) => {
+        res.forEach((request) => {
+          if (request.requested === currentUser._id && request.requester === person._id) {
+            // Remove friend request from DB
+            fetch(`${process.env.REACT_APP_API_DOMAIN}/friendRequests/${request._id}`, {
+              method: 'DELETE',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token.token}`
+              }
+            });
+          };
+        });
+      });
+    });
+  };
+
   useEffect(() => {
     // Make api call to get all users
     fetch(`${process.env.REACT_APP_API_DOMAIN}/users`,
@@ -74,12 +130,13 @@ function Users() {
             <div key={user._id} className='user'>
               <div className='user-image'>placeholder</div>
               <h5 className='user-name'>{`${user.firstName} ${user.lastName}`}</h5>
-              {
-                ((requestedFriends.data.includes(user._id) &&
+              {// so confusing... but it works
+                (token.user.friends.includes(user._id) && <p>friends</p>) || 
+                (((requestedFriends.data.includes(user._id) &&
                   <div className='requested-block'>Friend requested</div>) ||
                 (friendRequested.data.includes(user._id) &&
-                  <button className='friend-requested btn'>Accept request</button>)) ||
-                <button className='add-friend-button btn' onClick={() => requestFriend(user)}>Add friend</button> 
+                  <button className='friend-requested btn' onClick={() => acceptRequest(user)}>Accept request</button>)) ||
+                <button className='add-friend-button btn' onClick={() => requestFriend(user)}>Add friend</button> )
               }
             </div>
           );
