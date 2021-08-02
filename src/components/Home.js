4e6efa2d1f93018acf7d659a17e096219bc4ca9e
run-after <run.after@protonmail.com>
2021-08-02 10:52:12 -0500

    Fix error message for post form

--- src/components/Home.js
+++ src/components/Home.js
@@ -59,7 +59,7 @@ function Home() {
       <div className='left-column'>Left</div>
       <div className='post-feed'>
         <form className='post-form' onSubmit={createPost}>
-          <div className='text-area-container'>
+          <div className={`text-area-container ${errorMessage && 'highlight-error'}`}>
             {<div className='error-message'>{errorMessage}</div>}
             <textarea id='content' name='content' onChange={changeText} value={textAreaText} placeholder='What is on your mind?' required></textarea>
           </div>
@@ -78,5 +78,3 @@ export default Home;
 
 // When i add token.user.friends to dependency array on useEffect it keeps
 // making calls to api
-
-// Don't love error message for post form
